18ae2a2bd843cecc2b01681cfc37a367050b19f5
Marine Miyuku HEMMERT <marine.hemmert@gmail.com>
2015-11-22 20:10:45 +0100

    Résolution des anomalies mineures suivantes : - GS96 (Dans la consultation des frais, le montant est affiché avec un point à la place d'une virgule.), - GS97 (Des fautes de français dans la consultation des frais), - GS99 (Pour les frais hors forfait, la saisie d'un libellé long est tronquée.).

---
+++ controleurs/c_connexion.php
@@ -0,0 +1,36 @@
+﻿<?php
+if (!isset($_REQUEST['action'])) {
+    $_REQUEST['action'] = 'demandeconnexion';
+}
+$action = $_REQUEST['action'];
+switch ($action) {
+    case 'demandeConnexion': {
+            include("vues/v_connexion.php");
+            break;
+        }
+    case 'valideConnexion': {
+            $login = $_REQUEST['login'];
+            $mdp = $_REQUEST['mdp'];
+            $visiteur = $pdo->getInfosVisiteur($login, $mdp);
+            if (!is_array($visiteur)) {
+                ajouterErreur("Login ou mot de passe incorrect");
+                include("vues/v_erreurs.php");
+                include("vues/v_connexion.php");
+            } else {
+                $id = $visiteur['id'];
+                $nom = $visiteur['nom'];
+                $prenom = $visiteur['prenom'];
+                connecter($id, $nom, $prenom);
+                // fix header redirection
+                echo '<script>window.location.replace("index.php");</script>';
+                // error the header must be the first before the html
+                //header('location: index.php');
+            }
+            break;
+        }
+    default : {
+            include("vues/v_connexion.php");
+            break;
+        }
+}
+?>
